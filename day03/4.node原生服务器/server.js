c4e6e39a7a4de1ced8600b3609751c4d5fe77a59
tianyu <zhangtianyu5177@sina.com>
2019-08-23 09:36:24 +0800

    update

--- day03/4.node原生服务器/server.js
+++ day03/4.node原生服务器/server.js
@@ -4,6 +4,8 @@
 
 //引入Node内置的http模块
 let http = require('http')
+//引入Node中内置的querystring，用于把客户端携带过来的查询字符串参数，转成对象
+let {parse} = require('querystring')
 
 //创建服务对象
 let server = http.createServer(function (request,response) {
@@ -11,8 +13,11 @@ let server = http.createServer(function (request,response) {
   * request对象：包含着客户端发过来的数据
   * response对象：要交给客户端的数据
   * */
+  let stParams = request.url.split('?')[1]
+  let objParams = parse(stParams)
+  console.log(objParams);
   response.setHeader('content-type','text/html;charset=utf-8')
-  response.end('<h1>要放学了</h1>')
+  response.end(`<h1>你的名字是${objParams.name}，你的年龄是${objParams.age}</h1>`)
 })
 
 //绑定一个端口运行
